d22b7af60c324eeb100ace01381c32e1fd1b417b
thaoHT <thaothuhoang24@gmail.com>
2026-01-05 00:13:32 +0700

    Initial commit

---
+++ hauiTrash/src/test/java/com/example/hauiTrash/HauiTrashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hauiTrash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HauiTrashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
